a4de6e3038dfa0e7bba6a1b6152b27dd7f7d1ee5
m.rezvani <mohsen.rezvani.rad33@gmail.com>
2023-10-23 16:28:15 +0330

    first commit

---
+++ src/_metronic/helpers/crud-helper/models.ts
@@ -0,0 +1,120 @@
+import {Dispatch, SetStateAction} from 'react'
+
+export type ID = undefined | null | number
+
+export type PaginationState = {
+  page: number
+  items_per_page: 10 | 30 | 50 | 100
+  links?: Array<{label: string; active: boolean; url: string | null; page: number | null}>
+}
+
+export type SortState = {
+  sort?: string
+  order?: 'asc' | 'desc'
+}
+
+export type FilterState = {
+  filter?: unknown
+}
+
+export type SearchState = {
+  search?: string
+}
+
+export type Response<T> = {
+  data?: T
+  payload?: {
+    message?: string
+    errors?: {
+      [key: string]: Array<string>
+    }
+    pagination?: PaginationState
+  }
+}
+
+export type QueryState = PaginationState & SortState & FilterState & SearchState
+
+export type QueryRequestContextProps = {
+  state: QueryState
+  updateState: (updates: Partial<QueryState>) => void
+}
+
+export const initialQueryState: QueryState = {
+  page: 1,
+  items_per_page: 10,
+}
+
+export const initialQueryRequest: QueryRequestContextProps = {
+  state: initialQueryState,
+  updateState: () => {},
+}
+
+export type QueryResponseContextProps<T> = {
+  response?: Response<Array<T>> | undefined
+  refetch: () => void
+  isLoading: boolean
+  query: string
+}
+
+export const initialQueryResponse = {refetch: () => {}, isLoading: false, query: ''}
+
+export type ListViewContextProps = {
+  selected: Array<ID>
+  onSelect: (selectedId: ID) => void
+  onSelectAll: () => void
+  clearSelected: () => void
+  // NULL => (CREATION MODE) | MODAL IS OPENED
+  // NUMBER => (EDIT MODE) | MODAL IS OPENED
+  // UNDEFINED => MODAL IS CLOSED
+  itemIdForUpdate?: ID
+  setItemIdForUpdate: Dispatch<SetStateAction<ID>>
+  isAllSelected: boolean
+  disabled: boolean
+}
+
+export const initialListView: ListViewContextProps = {
+  selected: [],
+  onSelect: () => {},
+  onSelectAll: () => {},
+  clearSelected: () => {},
+  setItemIdForUpdate: () => {},
+  isAllSelected: false,
+  disabled: false,
+}
+
+interface ServerFieldError {
+  email?: string[]
+  password?: string[]
+  // ... other fields ...
+}
+
+export interface CustomError {
+  code: number
+  message: string
+  errors: ServerFieldError
+}
+
+export interface ErrorResponse {
+  response: {
+    data: CustomError
+  }
+}
+
+export function isCustomError(error: unknown): error is ErrorResponse {
+  // We assert 'error' is an object with a 'response' key, giving TypeScript the context it needs.
+  const errorAsObject = error as {response?: unknown}
+
+  return (
+    typeof errorAsObject === 'object' &&
+    errorAsObject !== null && // because 'typeof null' also returns 'object'
+    typeof errorAsObject.response === 'object' &&
+    errorAsObject.response !== null &&
+    typeof (errorAsObject.response as {data?: unknown}).data === 'object' &&
+    (errorAsObject.response as {data?: unknown}).data !== null &&
+    // Now, we perform our specific checks to ensure it matches the CustomError structure
+    typeof ((errorAsObject.response as {data?: unknown}).data as CustomError).code === 'number' &&
+    typeof ((errorAsObject.response as {data?: unknown}).data as CustomError).message ===
+      'string' &&
+    typeof ((errorAsObject.response as {data?: unknown}).data as CustomError).errors === 'object'
+  )
+}
---
+++ src/app/modules/auth/components/Login.tsx
@@ -0,0 +1,214 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import {useState} from 'react'
+import * as Yup from 'yup'
+import clsx from 'clsx'
+import {Link} from 'react-router-dom'
+import {useFormik} from 'formik'
+import {getUserByToken, login} from '../core/_requests'
+import {isCustomError, toAbsoluteUrl} from '../../../../_metronic/helpers'
+import {useAuth} from '../core/Auth'
+import {useIntl} from 'react-intl'
+
+const loginSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('فرمت ایمیل اشتباه است')
+    .min(3, 'حداقل باید سه کارکتر باشد')
+    .max(50, 'حداکثر باید ۵۰ کارکتر باشد')
+    .required('ایمیل اجباری است'),
+  password: Yup.string()
+    .min(3, 'حداقل باید سه کارکتر باشد')
+    .max(50, 'حداکثر باید ۵۰ کارکتر باشد')
+    .required('رمزعبور اجباری است'),
+})
+
+const initialValues = {
+  email: 'admin@demo.com',
+  password: 'demo',
+}
+
+/*
+  Formik+YUP+Typescript:
+  https://jaredpalmer.com/formik/docs/tutorial#getfieldprops
+  https://medium.com/@maurice.de.beijer/yup-validation-and-typescript-and-formik-6c342578a20e
+*/
+
+export function Login() {
+  const intl = useIntl()
+
+  const [loading, setLoading] = useState(false)
+  const {saveAuth, setCurrentUser} = useAuth()
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema: loginSchema,
+    onSubmit: async (values, {setStatus, setSubmitting, setFieldError}) => {
+      setLoading(true)
+      try {
+        debugger
+        const {data: auth} = await login(values.email, values.password)
+        saveAuth(auth)
+        const {data: user} = await getUserByToken(auth.data.api_token)
+        setCurrentUser(user)
+      } catch (error) {
+        console.error(error)
+        saveAuth(undefined)
+        setSubmitting(false)
+        setLoading(false)
+
+        // Check if the error is a custom error with a response field
+        if (isCustomError(error)) {
+          const errorMessage = error.response.data.message
+          setStatus(errorMessage)
+
+          // Check if the error contains form field validations
+          const fieldErrors = error.response.data.errors
+          if (fieldErrors) {
+            // If there's a specific error message for 'email', set it
+            if (fieldErrors.email && fieldErrors.email.length > 0) {
+              setFieldError('email', fieldErrors.email.join(','))
+            }
+            if (fieldErrors.password && fieldErrors.password.length > 0) {
+              setFieldError('password', fieldErrors.password.join(','))
+            }
+            // ... repeat for other fields if necessary ...
+          }
+        } else {
+          setStatus('An unexpected error occurred.')
+        }
+      }
+    },
+  })
+
+  return (
+    <form
+      className='form w-100'
+      onSubmit={formik.handleSubmit}
+      noValidate
+      id='kt_login_signin_form'
+    >
+      {/* begin::Heading */}
+      <div className='text-center mb-11'>
+        <h1 className='text-dark fw-bolder mb-3'>
+          {' '}
+          {intl.formatMessage({id: 'AUTH.LOGIN.BUTTON'})}
+        </h1>
+      </div>
+      {/* begin::Heading */}
+
+      {/* begin::Login options */}
+
+      {/* end::Login options */}
+
+      {/* begin::Separator */}
+
+      {/* end::Separator */}
+
+      {formik.status && (
+        <div className='mb-lg-15 alert alert-danger'>
+          <div className='alert-text font-weight-bold'>{formik.status}</div>
+        </div>
+      )}
+
+      {/* begin::Form group */}
+      <div className='fv-row mb-8'>
+        <label className='form-label fs-6 fw-bolder text-dark'>
+          {' '}
+          {intl.formatMessage({id: 'AUTH.INPUT.EMAIL'})}
+        </label>
+        <input
+          placeholder='Email'
+          {...formik.getFieldProps('email')}
+          className={clsx(
+            'form-control bg-transparent',
+            {'is-invalid': formik.touched.email && formik.errors.email},
+            {
+              'is-valid': formik.touched.email && !formik.errors.email,
+            }
+          )}
+          type='email'
+          name='email'
+          autoComplete='off'
+        />
+        {formik.touched.email && formik.errors.email && (
+          <div className='fv-plugins-message-container'>
+            <span role='alert'>{formik.errors.email}</span>
+          </div>
+        )}
+      </div>
+      {/* end::Form group */}
+
+      {/* begin::Form group */}
+      <div className='fv-row mb-3'>
+        <label className='form-label fw-bolder text-dark fs-6 mb-0'>
+          {' '}
+          {intl.formatMessage({id: 'AUTH.INPUT.PASSWORD'})}
+        </label>
+        <input
+          type='password'
+          autoComplete='off'
+          {...formik.getFieldProps('password')}
+          className={clsx(
+            'form-control bg-transparent',
+            {
+              'is-invalid': formik.touched.password && formik.errors.password,
+            },
+            {
+              'is-valid': formik.touched.password && !formik.errors.password,
+            }
+          )}
+        />
+        {formik.touched.password && formik.errors.password && (
+          <div className='fv-plugins-message-container'>
+            <div className='fv-help-block'>
+              <span role='alert'>{formik.errors.password}</span>
+            </div>
+          </div>
+        )}
+      </div>
+      {/* end::Form group */}
+
+      {/* begin::Wrapper */}
+      <div className='d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8'>
+        <div />
+
+        {/* begin::Link */}
+        <Link to='/auth/forgot-password' className='link-primary'>
+          {intl.formatMessage({id: 'AUTH.GENERAL.FORGOT_BUTTON'})} ؟
+        </Link>
+        {/* end::Link */}
+      </div>
+      {/* end::Wrapper */}
+
+      {/* begin::Action */}
+      <div className='d-grid mb-10'>
+        <button
+          type='submit'
+          id='kt_sign_in_submit'
+          className='btn btn-primary'
+          disabled={formik.isSubmitting || !formik.isValid}
+        >
+          {!loading && (
+            <span className='indicator-label'>
+              {' '}
+              {intl.formatMessage({id: 'AUTH.CONTINUE.BUTTON'})}
+            </span>
+          )}
+          {loading && (
+            <span className='indicator-progress' style={{display: 'block'}}>
+              Please wait...
+              <span className='spinner-border spinner-border-sm align-middle ms-2'></span>
+            </span>
+          )}
+        </button>
+      </div>
+      {/* end::Action */}
+
+      <div className='text-gray-500 text-center fw-semibold fs-6'>
+        {intl.formatMessage({id: 'AUTH.GENERAL.NO_ACCOUNT'})}{' '}
+        <Link to='/auth/registration' className='link-primary'>
+          {intl.formatMessage({id: 'AUTH.REGISTER.TITLE'})}
+        </Link>
+      </div>
+    </form>
+  )
+}
